c31e42726b246971526931e036691a352a411904
Prince Kayii <asiamahkayi@gmail.com>
2022-02-10 14:49:50 +0200

    updated version-1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Switch,Route } from 'react-router-dom';
+import SideNav from './SideNav/SideNav';
+import SideFoot from './SideNav/SideFoot';
+import './SideNav/SideNav.css'
+import './SideNav/SideFoot.css'
+import Contact from './Hero/Contact';
+import Footer from   './Hero/Footer'
+import NavSide from './Navigation/NavSide';
+import './Navigation/NavSide.css';
+import Home from'./Pages/Home';
+import Products from './Pages/Products';
+import Reports from './Pages/Reports';
+import Console from './Pages/Console';
+import Sign from './Pages/Sign';
+import './App.css'
 
-import Footer from './components/Footer';
+
+ function App(){
+
+
+return(
+
+<div className='App'>
+ 
+ <Router>
+ <NavSide />
+
+
+ <Switch>
+   <Route path="/"  exact component={Home}/>
+ <Route path="/products"  component={Products}/>
+ <Route path="/reports" component={Reports}/>
+ <Route path="/console" component={Console}/>
+
+ <Route path="/sign" component={Sign}/>
 
 
 
-function App() {
-  return (
-    <div>
+
+
+ </Switch>
 
    
-    </div>
+   </Router> 
+
+</div>
+  
   );
 }
 
